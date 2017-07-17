a9d895f4e72813aa507b6712a105b2f85b469fd4
hhstore <hhstore@qq.com>
2017-07-17 17:10:25 +0000

    update - 辅助 <router-view> 标签, 结合 vue-router 实现对组件的路由匹配.

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-//
+// ******************************************
+//             组件列表:
+// ******************************************
 import Hello from '@/components/Hello'
 import Resume from '@/components/Resume'
 
 //
 Vue.use(Router)
 
+// ******************************************
+//             路由匹配:
+// 注意:
+//    - 与 <router-view> 标签关联, 黑盒部分
+//    - 组件的路由搜索和匹配, 都是在这里设置的
+//
+// ******************************************
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello
     },
     {
       path: '/',
-      name: 'Resume',
+      name: 'Resume',   // 简历组件
       component: Resume
     }
   ]
