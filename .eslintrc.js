9eb3ae9364af0ffaf966d9b495685e0bd536107c
Henry.Huang <hhglory@outlook.com>
2024-04-09 12:23:56 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    // todo x: fix
+    "space-before-function-paren": 0 // off
   }
 }
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -15,22 +15,24 @@
     <!--姓名-->
     <div class="ui grid left aligned">
       <div class="twelve wide column">
-        <h1 class="ui teal header"> {{cv.profile.name}} </h1>
+        <h1 class="ui teal header"> {{ cv.profile.name }} </h1>
       </div>
       <div class="four wide column right aligned">
-        <h3>期望薪资: {{cv.profile.expect.salary}}</h3>
+        <h3>期望薪资: {{ cv.profile.expect.salary }}</h3>
       </div>
     </div>
 
     <!--联系方式-->
     <table class="ui table teal cv-table">
-      <thead><tr></tr></thead>
+      <thead>
+      <tr></tr>
+      </thead>
 
       <!--<tbody>-->
-        <!--<tr>-->
-          <!--<td colspan="3"><h1 class="ui teal header"> {{cv.profile.name}} </h1></td>-->
-          <!--<td class="right aligned"><h3>期望薪资: {{cv.profile.expect.salary}}</h3></td>-->
-        <!--</tr>-->
+      <!--<tr>-->
+      <!--<td colspan="3"><h1 class="ui teal header"> {{cv.profile.name}} </h1></td>-->
+      <!--<td class="right aligned"><h3>期望薪资: {{cv.profile.expect.salary}}</h3></td>-->
+      <!--</tr>-->
       <!--</tbody>-->
 
       <tbody>
@@ -71,13 +73,13 @@
         <td>
           <h4>
             <i class="call icon teal"></i>
-            {{cv.profile.mobile}}
+            {{ cv.profile.mobile }}
           </h4>
         </td>
         <td>
           <h4>
             <i class="mail icon teal"></i>
-            <a class="header" href="#">{{cv.profile.email}}</a>
+            <a class="header" href="#">{{ cv.profile.email }}</a>
           </h4>
         </td>
         <td>
@@ -103,7 +105,9 @@
 
     <!--学历信息-->
     <table class="ui table teal cv-table ">
-      <thead><tr></tr></thead>
+      <thead>
+      <tr></tr>
+      </thead>
       <tbody>
       <tr>
         <td>
@@ -125,7 +129,11 @@
 
     <!--工作经历-->
     <table class="ui table teal cv-tb-job">
-      <thead><tr><td></td></tr></thead>
+      <thead>
+      <tr>
+        <td></td>
+      </tr>
+      </thead>
       <!--单独的标题项-->
       <tbody>
       <tr>
@@ -165,58 +173,65 @@
     <!--企业开发项目列表-->
     <table class="ui table teal cv-tb-job">
       <thead>
-        <tr><td></td></tr>
+      <tr>
+        <td></td>
+      </tr>
       </thead>
       <!--单独的标题项-->
       <tbody>
-        <tr>
-          <h3 class="ui blue header">
-            <i class="small cubes icon teal"></i>企业项目
-          </h3>
-        </tr>
+      <tr>
+        <h3 class="ui blue header">
+          <i class="small cubes icon teal"></i>企业项目
+        </h3>
+      </tr>
       </tbody>
 
       <tbody v-for="item in cv.project.working">
-        <tr>
-          <!--项目名称-->
-          <td><h4><i class="cube icon teal"></i><a target="_blank" v-bind:href="item.url">{{ item.name }}</a></h4></td>
-          <!--项目官网-->
-          <td><h4><a target="_blank" v-bind:href="item.url"><i class="internet explorer icon teal"></i>项目官网</a></h4></td>
-          <!--项目周期-->
-          <td class="right aligned"><h4>{{ item.duration }}</h4></td>
-          <!--担任职位-->
-          <td class="right aligned"><h4><i class="user icon teal"></i>{{ item.title }}</h4></td>
-          <!--所在企业-->
-          <td class="right aligned"><h4>{{ item.company }}</h4></td>
-        </tr>
+      <tr>
+        <!--项目名称-->
+        <td><h4><i class="cube icon teal"></i><a target="_blank" v-bind:href="item.url">{{ item.name }}</a></h4></td>
+        <!--项目官网-->
+        <td><h4><a target="_blank" v-bind:href="item.url"><i class="internet explorer icon teal"></i>项目官网</a></h4>
+        </td>
+        <!--项目周期-->
+        <td class="right aligned"><h4>{{ item.duration }}</h4></td>
+        <!--担任职位-->
+        <td class="right aligned"><h4><i class="user icon teal"></i>{{ item.title }}</h4></td>
+        <!--所在企业-->
+        <td class="right aligned"><h4>{{ item.company }}</h4></td>
+      </tr>
 
-        <!--项目简介-->
-        <tr>
-          <td colspan="5">
-            <!--简介-->
-            <h5 class="cv-fix-content-tab4"><i class="bell teal icon"></i>简介: {{ item.brief }}</h5>
-            <!--项目技术栈-->
-            <h5 class="cv-fix-content-tab4"><i class="bell teal icon"></i>技术栈: <u>{{ item.tech_architecture }}</u></h5>
+      <!--项目简介-->
+      <tr>
+        <td colspan="5">
+          <!--简介-->
+          <h5 class="cv-fix-content-tab4"><i class="bell teal icon"></i>简介: {{ item.brief }}</h5>
+          <!--项目技术栈-->
+          <h5 class="cv-fix-content-tab4"><i class="bell teal icon"></i>技术栈: <u>{{ item.tech_architecture }}</u></h5>
 
-            <!--技术成果-->
-            <div v-for="v in item.content">
-              <h5 class="cv-fix-content-tab4"><i class="user teal icon"></i>{{ v.item }}</h5>
-              <div class="cv-fix-content-tab8" v-for="vv in v.desc">
-                <h5><i class="pointing right purple icon"></i>{{ vv }}</h5>
-              </div>
+          <!--技术成果-->
+          <div v-for="v in item.content">
+            <h5 class="cv-fix-content-tab4"><i class="user teal icon"></i>{{ v.item }}</h5>
+            <div class="cv-fix-content-tab8" v-for="vv in v.desc">
+              <h5><i class="pointing right purple icon"></i>{{ vv }}</h5>
             </div>
+          </div>
 
-            <!--控制项目之间分割-->
-            <br>
-          </td>
-        </tr>
+          <!--控制项目之间分割-->
+          <br>
+        </td>
+      </tr>
       </tbody>
     </table>
 
 
     <!--业余兼职项目列表-->
     <table class="ui table teal cv-tb-job">
-      <thead><tr><td></td></tr></thead>
+      <thead>
+      <tr>
+        <td></td>
+      </tr>
+      </thead>
       <!--单独的标题项-->
       <tbody>
       <tr>
@@ -231,7 +246,8 @@
         <!--项目名称-->
         <td><h4><i class="cube icon teal"></i><a target="_blank" v-bind:href="item.url">{{ item.name }}</a></h4></td>
         <!--项目官网-->
-        <td><h4><a target="_blank" v-bind:href="item.url"><i class="internet explorer icon teal"></i>项目官网</a></h4></td>
+        <td><h4><a target="_blank" v-bind:href="item.url"><i class="internet explorer icon teal"></i>项目官网</a></h4>
+        </td>
         <!--项目周期-->
         <td><h4>{{ item.duration }}</h4></td>
         <!--担任职位-->
@@ -265,8 +281,12 @@
 
 
     <!--开源项目列表-->
-    <table  class="ui table teal cv-tb-job">
-      <thead><tr><td></td></tr></thead>
+    <table class="ui table teal cv-tb-job">
+      <thead>
+      <tr>
+        <td></td>
+      </tr>
+      </thead>
       <!--单独的标题项-->
       <tbody>
       <tr>
@@ -277,25 +297,30 @@
       </tbody>
 
       <tbody>
-        <tr v-for="item in cv.project.personal">
-          <!--项目访问地址-->
-          <td>
-            <h4><i class="github icon teal"></i><a target="_blank" v-bind:href="'https://'+item.url">{{ item.url
-              }}</a>
-            </h4>
-          </td>
-          <!--项目类型-->
-          <td><h4>{{ item.tech_main_type }}</h4></td>
-          <!--项目简介-->
-          <td><h4><i class="pointing right purple icon"></i>{{ item.brief }}</h4></td>
-        </tr>
+      <tr v-for="item in cv.project.personal">
+        <!--项目访问地址-->
+        <td>
+          <h4><i class="github icon teal"></i><a target="_blank" v-bind:href="'https://'+item.url">{{
+              item.url
+            }}</a>
+          </h4>
+        </td>
+        <!--项目类型-->
+        <td><h4>{{ item.tech_main_type }}</h4></td>
+        <!--项目简介-->
+        <td><h4><i class="pointing right purple icon"></i>{{ item.brief }}</h4></td>
+      </tr>
       </tbody>
     </table>
 
 
     <!--其他说明-->
-    <table  class="ui table teal cv-tb-job">
-      <thead><tr><td></td></tr></thead>
+    <table class="ui table teal cv-tb-job">
+      <thead>
+      <tr>
+        <td></td>
+      </tr>
+      </thead>
       <!--单独的标题项-->
       <tbody>
       <tr>
@@ -306,16 +331,16 @@
       </tbody>
 
       <tbody>
-        <tr v-for="item in cv.etc">
-          <td>
-            <!--分类-->
-            <h4><i class="user teal icon"></i>{{item.title }}:</h4>
-            <div v-for="v in item.desc">
-              <!--详情-->
-              <h5 class="cv-fix-content-tab8"><i class="pointing right purple icon"></i>{{ v }}</h5>
-            </div>
-          </td>
-        </tr>
+      <tr v-for="item in cv.etc">
+        <td>
+          <!--分类-->
+          <h4><i class="user teal icon"></i>{{ item.title }}:</h4>
+          <div v-for="v in item.desc">
+            <!--详情-->
+            <h5 class="cv-fix-content-tab8"><i class="pointing right purple icon"></i>{{ v }}</h5>
+          </div>
+        </td>
+      </tr>
       </tbody>
     </table>
 
@@ -324,33 +349,33 @@
 
 
 <style scoped lang="less">
-  @import "./Resume.less";
+@import "./Resume.less";
 </style>
 
 
 <script>
-  // import * as cv from '../assets/cv.json'    // cv-2017.json
-  import * as cv from '../assets/cv.json'    // cv-2017.json
+// import * as cv from '../assets/cv.json'    // cv-2017.json
+import * as cv from '../assets/cv.json'    // cv-2017.json
 
-  export default {
-    name: 'resume',
-    components: {},
+export default {
+  name: 'resume',
+  components: {},
 
-    data () {
-      return {
-        cv: cv
-      }
-    },
+  data() {
+    return {
+      cv: cv
+    }
+  },
 
-    methods: {
-      exportPDF: function () {
-        // 隐藏打印按钮
-        document.getElementById('cv-export').style.display = 'none'
-        window.print()
-        // 恢复打印按钮
-        document.getElementById('cv-export').style.display = ''
-      }
+  methods: {
+    exportPDF: function () {
+      // 隐藏打印按钮
+      document.getElementById('cv-export').style.display = 'none'
+      window.print()
+      // 恢复打印按钮
+      document.getElementById('cv-export').style.display = ''
     }
   }
+}
 
 </script>
